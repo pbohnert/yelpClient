e3a7ecee16c69acd7f4a51f532b3c82272294f29
Peter Bohnert <pbohnert@gmail.com>
2014-10-02 13:34:49 -0700

    added in api call to Yelp to return data. Also initialized and installed my Podfile and Cocoapods libraries.

--- yelpClient/ViewController.swift
+++ yelpClient/ViewController.swift
@@ -12,30 +12,76 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
-    var places: [[String:String]]!
+    var client: YelpClient!
+    
+    //var places: [[String:String]]!
+     var places: [NSDictionary] = []
+    
+    // my Yelp keys
+    let yelpConsumerKey = "WxM1ynG7wdd032qyipfagQ"
+    let yelpConsumerSecret = "8lrED6F1gWOb1XSwr3FfkSbeNX8"
+    let yelpToken = "J89lj9umCgqWyfByF6E6QsoTEZA01ejv"
+    let yelpTokenSecret = "Q82qHm43iwDe-2ECP6du7NplfRk"
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        places = [["name" : "Peter B", "address" : "1401 Park Ave"]]
+        client = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
         
-        tableView.delegate = self
-        tableView.dataSource = self
+        client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            //println(response)
+            println(response.count)
+            self.places = response["businesses"] as [NSDictionary]
+            //println(self.places)
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
+        }
+        
+
+  //      places = [["name" : "Peter B", "address" : "1401 Park Ave"]]
+        
+          tableView.delegate = self
+          tableView.dataSource = self
        // tableView.rowHeight = 107
-        
+            self.tableView.reloadData()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        println("places.count = \(places.count)")
         return places.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("PlaceCell") as PlaceCell
         
-        var place = places[indexPath.row]
-        cell.nameLabel.text = place["name"]
-        cell.address.text = place["address"]
+        self.client.searchWithTerm("Thai", success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
+            //println(response)
+            println(response.count)
+            self.places = response["businesses"] as [NSDictionary]
+            println(self.places)
+            
+            //name, address, ratings (count),
+            var place = self.places[indexPath.row]
+            println("am I even getting here?")
+            
+            var location = place["location"] as NSDictionary
+            cell.address.text = location["display_address"] as? String
+            cell.nameLabel.text = place["name"] as? String
+            var reviewCount = place["review_count"] as Int
+            cell.reviews.text = "\(reviewCount)"
+            
+            tableView.reloadData()
+            
+            }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
+                println(error)
+        }
+        
+        //set image here as well, for placeImage.
         
         return cell
     }
